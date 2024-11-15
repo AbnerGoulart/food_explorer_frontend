ffb6bef48c65525275ddca73e11c45f8b9d04d67
Abner Goulart <abnergoulart.10@gmail.com>
2024-11-15 14:52:34 -0300

    remoção do import das paginas no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import GlobalStyles from "./styles/global.js";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.js";
 import { CartProvider } from "./contexts/CartContext.jsx";
-
-import { SignIn } from "./pages/SignIn";
-import { SignUp } from "./pages/SignUp";
-
 import { Routes } from "./routes";
 
 createRoot(document.getElementById("root")).render(
