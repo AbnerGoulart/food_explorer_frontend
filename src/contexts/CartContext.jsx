2abba8ba0bce0e8f74d5ed2d0463fb9b22ad28da
Abner Goulart <abnergoulart.10@gmail.com>
2024-11-15 19:39:35 -0300

    gerindo o localstorage para salvar os dados do cart

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -3,11 +3,14 @@ import React, { createContext, useEffect, useState } from "react";
 export const CartContext = createContext();
 
 export function CartProvider({ children }) {
-  const [count, setCount] = useState(0);
-  const [items, setItems] = useState([]);
+  const [count, setCount] = useState();
+  const [items, setItems] = useState(localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []);
 
   useEffect(() => {
     setCount(items.length)
+    if (items.length > 0){
+      localStorage.setItem("cart", JSON.stringify(items)) 
+    }
   }, [items])
 
   return (
