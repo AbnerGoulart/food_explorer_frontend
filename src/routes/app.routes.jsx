3c82b676fba2e240f01ae63af54a9bce72947675
Abner Goulart <abnergoulart.10@gmail.com>
2024-11-25 12:02:29 -0300

    criação da pagina edit

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -4,6 +4,7 @@ import { Home } from '../pages/Home'
 import { Details } from '../pages/Details'
 import { Menu } from '../pages/Menu'
 import { NewDish } from '../pages/NewDish'
+import { EditDish } from '../pages/EditDish'
 
 export function AppRoutes(){
   return (
@@ -12,6 +13,7 @@ export function AppRoutes(){
       <Route path='/details/:id' element={<Details />} />
       <Route path='/menu' element={<Menu />} />
       <Route path='/new' element={<NewDish />} />
+      <Route path='/edit' element={<EditDish />} />
     </Routes>
   )
 }
