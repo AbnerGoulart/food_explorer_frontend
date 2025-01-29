03a050527f2e603e7b8a725bbb0d265039ed0308
Abner Goulart <abnergoulart.10@gmail.com>
2025-01-29 12:06:23 -0300

    criação do contrato para chamada no endpoint da API

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,33 +1,57 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonText";
 import { Logo } from "../../components/Logo";
 import { Container } from "./styles";
 import { Input } from "../../components/Input";
+import { api } from "../../api";
 
 export function SignUp() {
-  const [senha, setSenha] = useState("");
-  const [erroSenha, setErroSenha] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+
+  const navigate = useNavigate();
 
   const handleChangeSenha = (event) => {
-    const senhaDigitada = event.target.value;
-    setSenha(senhaDigitada);
+    const passwordEntered = event.target.value;
+    setPassword(passwordEntered);
 
-    if (senhaDigitada.length < 6) {
-      setErroSenha("A senha deve ter pelo menos 6 caracteres.");
+    if (passwordEntered.length < 6) {
+      setErrorPassword("A senha deve ter pelo menos 6 caracteres.");
     } else {
-      setErroSenha("");
+      setErrorPassword("");
     }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (senha.length >= 6) {
+    if (password.length >= 6) {
       alert("Conta criada com sucesso!");
     } else {
       alert("Corrija os erros antes de criar sua conta.");
     }
+  };
+
+  const handleSignUp = () => {
+    if (!name || !email || !password) {
+      alert("Preencha todos os campos");
+    }
+
+    api
+      .post("/signup", {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        alert("Erro ao cadastrar");
+      });
   };
 
   return (
@@ -39,19 +63,36 @@ export function SignUp() {
         <h1>Crie sua conta</h1>
         <form className="content" onSubmit={handleSubmit}>
           <p>Seu nome</p>
-          <Input type="text" placeholder="Exemplo: Maria Alice" required />
+          <Input
+            type="text"
+            placeholder="Exemplo: Maria Alice"
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
           <p>Email</p>
-          <Input type="email" placeholder="exemplo@email.com" required />
+          <Input
+            type="email"
+            placeholder="exemplo@email.com"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
           <p>Senha</p>
           <Input
             type="password"
             placeholder="No mínimo 6 caracteres"
-            value={senha}
+            value={password}
             onChange={handleChangeSenha}
             required
           />
-          {erroSenha && <p className="error" style={{ color: "red" }}>{erroSenha}</p>}
-          <Button title="Criar conta" type="submit" />
+          {errorPassword && (
+            <p 
+              className="error" 
+              style={{ color: "red" }}
+            >
+              {errorPassword}
+            </p>
+          )}
+          <Button title="Criar conta" type="submit" onClick={handleSignUp} />
           <Link to="/">
             <ButtonText>Já tenho uma conta</ButtonText>
           </Link>
