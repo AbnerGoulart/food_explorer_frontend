ff8a3447e209ae7254c8999b94a9d175e264add4
Abner Goulart <AbnerGoulart>
2025-02-12 13:23:43 -0300

    atualizando prato via form data

--- src/pages/EditDish/index.jsx
+++ src/pages/EditDish/index.jsx
@@ -54,12 +54,25 @@ export function EditDish() {
       photo,
     };
 
+    const dishUploadForm = new FormData();
+    dishUploadForm.append("photo", photo);
+    dishUploadForm.append("title", title);
+    dishUploadForm.append("section", section);
+    dishUploadForm.append("description", description);
+    dishUploadForm.append("price", price);
+    
     if (tags.length > 0) {
-      updatedDish.tags = tags
+      dishUploadForm.append("tags", tags);
     }
 
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
     try {
-      await api.put(`/dishes/${id}`, updatedDish);
+      await api.put(`/dishes/${id}`, dishUploadForm, config);
       navigate("/");
     } catch (error) {
       console.error("Erro ao atualizar o prato:", error);
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL = "https://food-explorer-backend-3dbs.onrender.com"
+// export const API_BASE_URL = "https://food-explorer-backend-3dbs.onrender.com"
+export const API_BASE_URL = "http://localhost:3333"
 
 export const api = axios.create({
   baseURL: API_BASE_URL
